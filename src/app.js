2715973f3cc57170a86aec228c12d4df4d18bf3c
Alba <allbaluccia@gmail.com>
2024-05-14 10:51:25 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,24 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
+  console.log("ejecutar");
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let arr = ["♦", "♥", "♠", "♣"];
+  let nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let numsRandom = Math.floor(Math.random() * nums.length);
+  let arrRandom = Math.floor(Math.random() * arr.length);
+
+  if (arr[arrRandom] == "♦" || arr[arrRandom] == "♥") {
+    document.querySelector("#card").classList.add("red");
+  }
+  let top = document.querySelector("#top-suit");
+  let number = document.querySelector("#number");
+  let bottom = document.querySelector("#bottom-suit");
+  top.innerHTML = arr[arrRandom];
+  number.innerHTML = nums[numsRandom];
+  bottom.innerHTML = top.innerHTML;
 };
